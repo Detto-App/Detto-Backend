5125f52f7aee7c5d6cf6c0631f088631874bc5a8
Vishwa-Raghavendra <42895647+Vishwa-Raghavendra@users.noreply.github.com>
2021-03-15 20:11:43 +0530

    First

---
+++ src/Application.kt
@@ -0,0 +1,63 @@
+package com.dettoapp
+
+import com.dettoapp.data.User
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.gson.*
+import io.ktor.features.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+import io.ktor.client.*
+import io.ktor.client.engine.jetty.*
+import io.ktor.request.ContentTransformationException
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+
+    val list2:ArrayList<User> = ArrayList()
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+        }
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+
+        get("/json/jackson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+
+        post("/vinay")
+        {
+            val users = try {
+                call.receive<User>()
+                //call.respond("Done")
+            }catch (e:ContentTransformationException)
+            {
+                call.respond(HttpStatusCode.BadGateway)
+                return@post
+            }
+            list2.add(users)
+            call.respond(HttpStatusCode.OK)
+        }
+
+        get("/data")
+        {
+            call.respond(HttpStatusCode.OK,list2)
+        }
+    }
+}
+
---
+++ src/data/User.kt
@@ -0,0 +1,5 @@
+package com.dettoapp.data
+
+data class User(val name:String,val type:String,val email:String)
+{
+}
