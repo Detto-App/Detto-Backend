6cd1447547ce7edb8ae6680d19f0e2b42d084766
Vishwa-Raghavendra <42895647+Vishwa-Raghavendra@users.noreply.github.com>
2021-03-21 00:28:41 +0530

    getDetails and UserID

--- src/routes/RegistrationRoute.kt
+++ src/routes/RegistrationRoute.kt
@@ -3,11 +3,15 @@ package com.dettoapp.routes
 import com.dettoapp.data.ReceivingUserModel
 import com.dettoapp.data.StudentModel
 import com.dettoapp.data.TeacherModel
+import io.ktor.application.ApplicationCall
 import io.ktor.application.call
+import io.ktor.client.request.request
 import io.ktor.features.ContentTransformationException
 import io.ktor.http.HttpStatusCode
+import io.ktor.request.header
 import io.ktor.request.receive
 import io.ktor.response.respond
+import io.ktor.response.respondText
 import io.ktor.routing.Route
 import io.ktor.routing.get
 import io.ktor.routing.post
@@ -28,14 +32,15 @@ fun Route.registerUser() {
     route("/studentRegis")
     {
         post {
-            val request = try {
+            val request2 = try {
+                println()
                 call.receive<StudentModel>()
             } catch (e: ContentTransformationException) {
                 call.respond(HttpStatusCode.BadRequest)
                 return@post
             }
-            students.insertOne(request)
-            call.respond(HttpStatusCode.OK)
+            students.insertOne(request2)
+            call.respond(HttpStatusCode.OK,call.request.headers["vid"] ?:"")
         }
     }
 
@@ -66,15 +71,29 @@ fun Route.registerUser() {
         }
     }
 
-//    route("/gett")
-//    {
-//        get {
-//            call.respond(HttpStatusCode.OK,teachersList)
-//        }
-//    }
+    route("/gett")
+    {
+        get {
+            try {
+                val list = teachers.find().toList()
+                call.respond(HttpStatusCode.OK,list)
+            }catch (e:Exception)
+            {
+                println(e.localizedMessage)
+                call.respond(HttpStatusCode.OK,e.localizedMessage)
+            }
+        }
+    }
 
     route("/getDetails/{email}")
     {
+        get {
+            val user = students.findOne(StudentModel::email eq call.parameters["email"])
 
+            if(user!=null)
+                call.respond(HttpStatusCode.OK,user)
+            else
+                call.respond(HttpStatusCode.BadRequest)
+        }
     }
 }
