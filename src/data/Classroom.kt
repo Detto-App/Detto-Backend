46525341a668773715e07b9b6fe6869f122e5637
Vishwa-Raghavendra <42895647+Vishwa-Raghavendra@users.noreply.github.com>
2021-03-24 18:11:08 +0530

    Classroom delete route

--- src/data/Classroom.kt
+++ src/data/Classroom.kt
@@ -2,7 +2,7 @@ package com.dettoapp.data
 
 import org.bson.codecs.pojo.annotations.BsonId
 
-class Classroom(val classroomname: String, val year: String, val section: String, @BsonId val classroomuid: String, val userid: String,val tname:Stringam)
+class Classroom(val classroomname: String, val year: String, val section: String, @BsonId val classroomuid: String, val userid: String,val tname:String)
  {
 
 }
