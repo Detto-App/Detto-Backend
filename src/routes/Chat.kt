039c8048455742f5d662f2186333f4405927a90e
Vishwa-Raghavendra <42895647+Vishwa-Raghavendra@users.noreply.github.com>
2021-04-08 19:33:39 +0530

    Improvement : ChatWebSocket code improvements

--- src/routes/Chat.kt
+++ src/routes/Chat.kt
@@ -2,6 +2,7 @@ package com.dettoapp.routes
 
 import com.dettoapp.data.Chat
 import com.dettoapp.data.ChatMessages
+import com.dettoapp.data.CircularList
 import com.dettoapp.data.Connection
 import com.dettoapp.sendText
 import io.ktor.application.call
@@ -12,7 +13,10 @@ import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
 import io.ktor.routing.route
-import io.ktor.websocket.*
+import io.ktor.websocket.webSocket
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.litote.kmongo.eq
 import org.litote.kmongo.setValue
 import java.time.Instant
@@ -21,9 +25,8 @@ import java.util.*
 import kotlin.collections.HashMap
 import kotlin.collections.LinkedHashSet
 
-
 val connectionsHashMap = HashMap<String, MutableSet<Connection?>>()
-val chatMessagesHashMap = HashMap<String, ChatMessages>()
+val chatMessagesHashMap = HashMap<String, CircularList<Chat>>()
 
 fun Route.chat() {
 
@@ -47,51 +50,29 @@ fun Route.chat() {
             call.respond(HttpStatusCode.OK, chatMessageCollection.find().toList())
         }
     }
+
     route("/chat/{id}")
     {
         webSocket {
             val id = call.parameters["id"]!!
             val connections: MutableSet<Connection?>
-            var chatMessages: ChatMessages? = null
-
-            if (connectionsHashMap[id] == null) {
-                connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
-                connectionsHashMap[id] = connections
-                //println("Count inside NULL " + connectionsHashMap.count())
-            } else {
-                //println("Count is " + connectionsHashMap.count())
-                connections = connectionsHashMap[id]!!
-            }
-
+            val chatMessages: CircularList<Chat>
             val thisConnection = Connection(this)
 
+            connections = getConnection(id)
+            chatMessages = getChatMessageList(id)
+
             try {
-                //println("Adding user!")
                 connections += thisConnection
-                //println("Connects Count " + connections.size)
-
-                if (chatMessagesHashMap[id] == null) {
-                    chatMessages = chatMessageCollection.findOne(ChatMessages::pid eq id)
-                    if (chatMessages == null) {
-                        chatMessages = ChatMessages(id)
-                        chatMessagesHashMap[id] = chatMessages
-                        chatMessageCollection.insertOne(chatMessages)
-                    }
-                } else {
-                    chatMessages = chatMessagesHashMap[id]
+                chatMessages.forEach { data ->
+                    sendText(data.message)
                 }
 
-                val list = chatMessages!!.messages.getList(true)
-                list.forEach { data ->
-                    thisConnection.session.sendText(data.message)
-                }
-
-                val messages = chatMessages.messages
                 sendText("You are connected! There are ${connections.count()} users here.")
                 for (frame in incoming) {
                     frame as? Frame.Text ?: continue
                     val receivedText = frame.readText()
-                    messages.add(Chat(DateTimeFormatter.ISO_INSTANT.format(Instant.now()), receivedText))
+                    storeAndUpdateChatMessage(id, receivedText, chatMessages)
                     connections.forEach {
                         if (it != thisConnection) {
                             it?.session?.sendText(receivedText)
@@ -101,15 +82,44 @@ fun Route.chat() {
             } catch (e: Exception) {
                 sendText("" + e.localizedMessage)
             } finally {
-                //println("Removing $thisConnection!")
                 connections -= thisConnection
-                //println("Connects Count " + connections.size)
                 if (connections.size == 0) {
-                    chatMessageCollection.updateOne(ChatMessages::pid eq id, setValue(ChatMessages::messages, chatMessages!!.messages))
-                    connectionsHashMap.remove(id)
+                    CoroutineScope(Dispatchers.IO).launch {
+                        chatMessageCollection.updateOne(ChatMessages::pid eq id, setValue(ChatMessages::messages, chatMessages))
+                    }
                     chatMessagesHashMap.remove(id)
+                    connectionsHashMap.remove(id)
                 }
             }
         }
     }
+}
+
+private fun getConnection(id: String): MutableSet<Connection?> {
+    return if (connectionsHashMap[id] == null) {
+        val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
+        connectionsHashMap[id] = connections
+        connections
+    } else {
+        connectionsHashMap[id]!!
+    }
+}
+
+private suspend fun getChatMessageList(id: String) = if (chatMessagesHashMap[id] == null) {
+    var localChatMessages = chatMessageCollection.findOne(ChatMessages::pid eq id)
+    if (localChatMessages == null) {
+        localChatMessages = ChatMessages(id)
+        chatMessagesHashMap[id] = localChatMessages.messages
+        CoroutineScope(Dispatchers.IO).launch {
+            chatMessageCollection.insertOne(localChatMessages)
+        }
+    }
+    localChatMessages.messages
+} else {
+    chatMessagesHashMap[id]!!
+}
+
+private fun storeAndUpdateChatMessage(id: String, message: String, chatMessages: CircularList<Chat>) {
+    chatMessages.add(Chat(DateTimeFormatter.ISO_INSTANT.format(Instant.now()), message))
+    chatMessagesHashMap[id] = chatMessages
 }
